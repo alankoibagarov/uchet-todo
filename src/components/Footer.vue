82addf0fb8d920b0a53b48a5e4911046c3ad72dd
Alan <akoibagaroff@gmail.com>
2021-01-31 20:43:44 +0600

    update footer ref

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <nav id="footer" class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Made by Alan Koibagarov</a>
+    <span class="navbar-brand">
+      Made by
+      <a
+        style="color: #fff; text-decoration: underline"
+        href="https://github.com/alankoibagarov/uchet-todo"
+      >
+        Alan Koibagarov
+      </a>
+    </span>
   </nav>
 </template>
 
